187cacbcd7b8e950f5189e85e72144f256d9133f
Vitor G. Vellozo <vitor.vellozo@gmail.com>
2012-08-21 21:19:01 -0300

    commit

---
+++ ContatosIP67/ContatosIP67/ContatosIP67/Contato.h
@@ -0,0 +1,28 @@
+//
+//  Contato.h
+//  ContatosIP67
+//
+//  Created by ios2971 on 21/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contato : NSObject
+
+//@property(nonatomic, strong) NSString* _nome;
+//@property(nonatomic, strong) NSString* _telefone;
+//@property(nonatomic, strong) NSString* _email;
+//@property(nonatomic, strong) NSString* _endereco;
+//@property(nonatomic, strong) NSString* _site;
+
+@property NSString* _nome;
+@property NSString* _telefone;
+@property NSString* _email;
+@property NSString* _endereco;
+@property NSString* _site;
+
+//- (NSString *) nome;
+//- (void) setNome : (NSString *)nome;
+
+@end
